98cc0508a8540f65f35b15f8e7875aa2b4609dc0
Eric Stofferahn <Eric.Stofferahn@noaa.gov>
2019-08-26 16:04:56 -0400

    Julia version getting further into implementation.

--- gen_resource_tag.jl
+++ gen_resource_tag.jl
@@ -1,3 +1,14 @@
+abstract type Layout end
+struct AtmLayout <: Layout
+    layout::Tuple{Int,Int}
+    AtmLayout(layout) = new(layout)
+end
+
+struct IceLayout <: Layout
+    layout::Tuple{Int,Int}
+    IceLayout(layout) = new(layout)
+end
+
 struct ResourceTagSetup
     nodes::Int
     ht::String
@@ -21,22 +32,23 @@ struct Cluster
     cores_per_node::Int
     ranks::Int
     layout_cores::Int
-    possible_atm_layouts
-    ice_layout::Tuple{Int, Int}
+    possible_atm_layouts::Array{AtmLayout}
+    ice_layout::IceLayout
     Cluster(ρ, κ, cores_per_node, ranks, layout_cores, possible_atm_layouts, ice_layout) = new(ρ, κ, cores_per_node, ranks, layout_cores, possible_atm_layouts, ice_layout)
 end
 
-determine_io(α::Int) = mod(α,4) == 0 ? 4 : mod(α,3) == 0 ? 3 : mod(α,2) == 0 ? 2 : 1
-atm_factors(χ::Int) = unique([round.(Int, (i,fld(χ,i))) for i=2:floor(sqrt(χ)) if mod(χ,i) == 0 && i < χ && ((fld(χ,i^2) < 5 && fld(χ,i^2) > 1) || i == 3)])
-ice_factors(χ::Int) = unique([round.(Int, (i,fld(χ,i))) for i=2:floor(sqrt(χ)) if i == 3])
-
+determine_io(α::AtmLayout) = mod(α.layout[2],4) == 0 ? 4 : mod(α.layout[2],3) == 0 ? 3 : mod(α.layout[2],2) == 0 ? 2 : 1
+determine_io(ι::IceLayout) = mod(ι.layout[1],4) == 0 ? 4 : mod(ι.layout[1],3) == 0 ? 3 : mo
 function calc_threads(σ::ResourceTagSetup)
     ht = σ.ht == "off" ? 1 : σ.ht == "on" ? 2 : throw(DomainError(σ.ht, "argument must be \"off\" or \"on\""))
     omp = σ.omp == "off" ? 1 : σ.omp == "on" ? 2 : throw(DomainError(σ.omp, "argument must be \"off\" or \"on\""))
     ht * omp
 end
 
-rt_name(σ::ResourceTagSetup) = "$(repr(σ.nodes))nodes_ht_$(σ.ht)_omp_$(σ.omp)" 
+atm_factors(χ::Int) = unique([round.(Int, (i,fld(χ,i))) for i=2:floor(sqrt(χ)) if mod(χ,i) == 0 && i < χ && ((fld(χ,i^2) < 5 && fld(χ,i^2) > 1) || i == 3)])
+ice_factors(χ::Int) = unique([round.(Int, (i,fld(χ,i))) for i=2:floor(sqrt(χ)) if i == 3])
+
+rt_name(σ::ResourceTagSetup) = "$(repr(σ.nodes))nodes_ht_$(σ.ht)_omp_$(σ.omp)"
 
 function clock_to_str(ν::Int)
     seconds = fld(540 * 60, ν)
@@ -48,15 +60,44 @@ end
 function make_cluster(ρ::ResourceTag, κ::String)
     cores_per_node = κ == "c3" ? 32 : κ == "c4" ? 36 : throw(DomainError(κ, "cluster not recognized, current clusters are \"c3\" and \"c4\"."))
     ranks = cores_per_node * ρ.setup.nodes
-    println(typeof(ranks))
     layout_cores = div(ranks, 6)
-    println(typeof(layout_cores))
-    possible_atm_layouts = atm_factors(layout_cores)
-    ice_layout = ice_factors(ranks)[1]
+    possible_atm_layouts = [AtmLayout(atm_fact) for atm_fact in atm_factors(layout_cores)]
+    ice_layout = IceLayout(ice_factors(ranks)[1])
     my_cluster = Cluster(ρ, κ, cores_per_node, ranks, layout_cores, possible_atm_layouts, ice_layout)
 end
 
-function make_resource_tag(ν::Int, η::String, ω::String) 
+make_io_layout(λ::Layout) = (1, determine_io(λ))
+
+#make_layout_string(λ::Layout) = $Layout.layout(1, determine_io(λ))
+
+#    def make_layout_strings(self):
+#        self.atm_layout_string = f'{str(self.atm_layout[0])},{str(self.atm_layout[1])}'
+#        self.ice_layout_string = f'{str(self.ice_layout[1])},{str(self.ice_layout[0])}'
+#        self.atm_io_layout_string = f'{str(self.atm_io_layout[0])},{str(self.atm_io_layout[1])}'
+#        self.ice_io_layout_string = f'{str(self.ice_io_layout[0])},{str(self.ice_io_layout[1])}'
+#
+function match_layouts(γ::Array{AtmLayout,1}, ϵ::Array{AtmLayout,1})
+    layout_diff = 99999
+    c3idx = 0
+    c4idx = 0
+    for (c3i, c3l) in enumerate(γ)
+        for (c4i, c4l) in enumerate(ϵ)
+            this_diff = abs(c3l.layout[1] - c4l.layout[1]) + abs(c3l.layout[2] - c4l.layout[2])
+            if this_diff < layout_diff
+                layout_diff = this_diff
+                c3idx = c3i
+                c4idx = c4i
+            end
+        end
+    end
+    α = γ[c3idx]
+    α_alt = collect(delete!(Set(γ),α))
+    β = ϵ[c4idx]
+    β_alt = collect(delete!(Set(ϵ),β))
+    return (α, α_alt, β, β_alt)
+end
+
+function make_resource_tag(ν::Int, η::String, ω::String)
     my_rts = ResourceTagSetup(ν, η, ω)
     threads = calc_threads(my_rts)
     name = rt_name(my_rts)
@@ -66,7 +107,9 @@ function make_resource_tag(ν::Int, η::String, ω::String)
     my_rt = ResourceTag(my_rts, threads, name, wallclock, prefix, suffix)
     c3 = make_cluster(my_rt, "c3")
     c4 = make_cluster(my_rt, "c4")
-#    matched_atm_layouts = self.match_layouts() 
+    c3_atm_layout, c3_alt_atm_layouts, c4_atm_layout, c4_alt_atm_layouts = match_layouts(c3.possible_atm_layouts, c4.possible_atm_layouts)
+    atm_io_layout = make_io_layout(c3_atm_layout)
+    ice_io_layout = make_io_layout(c3.ice_layout)
 end
 
 make_resource_tag(3, "off", "off")
@@ -87,7 +130,7 @@ println(clock_to_str(192))
 
 println(atm_factors(96))
 println(ice_factors(96))
-                                             
+
 
 #def atm_factors(n):
 #    return list(set((i, int(n//i)) for i in range(2, int(n**0.5) + 1) if n % i == 0 and i < n and ((n//(i**2) < 5 and n//(i**2) > 1) or i == 3)))
@@ -167,7 +210,7 @@ println(ice_factors(96))
 #        self.wallclock = self.clock_to_str()
 #        self.c3 = Cluster(self, "c3")
 #        self.c4 = Cluster(self, "c4")
-#        self.matched_atm_layouts = self.match_layouts() 
+#        self.matched_atm_layouts = self.match_layouts()
 #
 #    def clock_to_str(self):
 #        seconds = fld(540 * 60, nodes)
@@ -196,6 +239,11 @@ println(ice_factors(96))
 #    def resolve_cluster_layouts(self):
 #        self.c3.resolve_layouts()
 #        self.c4.resolve_layouts()
+function calc_threads(σ::ResourceTagSetup)
+    ht = σ.ht == "off" ? 1 : σ.ht == "on" ? 2 : throw(DomainError(σ.ht, "argument must be \"off\" or \"on\""))
+    omp = σ.omp == "off" ? 1 : σ.omp == "on" ? 2 : throw(DomainError(σ.omp, "argument must be \"off\" or \"on\""))
+    ht * omp
+end
 #
 #    def write_prefix(self):
 #        string_lines = []
@@ -233,4 +281,3 @@ println(ice_factors(96))
 #
 #if __name__ == '__main__':
 #    write_full()
-
